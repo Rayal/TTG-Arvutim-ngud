c15381e7c71e3b3bc138dc09868a334933a31343
Andreas Ragen Ayal <andreas.r.ayal@gmail.com>
2017-11-30 17:08:00 +0200

    Esimene Tund

---
+++ Esimene Tund/Assets/Scripts/SphereController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+	Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float vert = Input.GetAxis ("Vertical");
+		float hori = Input.GetAxis ("Horizontal");
+		rb.AddForce (hori, 0.0f, vert);
+	}
+}
